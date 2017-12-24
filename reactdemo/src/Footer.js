88c681f588a5d0db9effd5bcfef2223602dc3a44
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-24 01:11:27 -0500

    initial file push

---
+++ reactdemo/src/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+// const footerMessage = "Here is the footer!"
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="footer">Here this the footer</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Footer;
