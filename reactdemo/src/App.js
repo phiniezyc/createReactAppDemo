9c6bb9c15e5a3e291484d3abc5fa98673b69c209
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-24 02:19:56 -0500

    added components to new components folder

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
     render() {
