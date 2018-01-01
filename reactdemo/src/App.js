1488e9c804ecda5256e17c97bbf1a2d32d93cea6
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-01 04:31:59 -0500

    added search input functionality that shows what you typed on the screen

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 import Header from './components/Header';
 
 import Body from './components/Body';
 import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
 
@@ -14,7 +15,9 @@ class App extends Component {
                 {/* <h1 testhead='test head'>Testing </h1> */}
                 <Header/>
                 <Body test="This is how you pass a variable" /> {/* Must use a parent component to pass to a child component that way! */}
+                <SearchBar />
                 <Footer footertest ="this is to test our footer" />
+
 
             </div>
         );
---
+++ reactdemo/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            // Can set specific default values also!
+            input: "The text you type will show up here!",
+        };
+    }
+
+    render() {
+        return (
+            <div className="search">
+                {/* we can use this.state.input to reference but don't use to set!use setState */}
+                <h1> {this.state.input} </h1> 
+                <input onChange={event => this.setState({ input: event.target.value })}/> 
+            </div>
+        );
+    }
+}
+
+
+
+export default SearchBar;
